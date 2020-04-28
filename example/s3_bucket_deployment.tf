a12e62585676833cc6c8a3bb1857dd8c719dc0fd
bl3nto <marko.kapor@gmail.com>
2020-04-28 09:44:42 +0200

    final commit, testing completed succesfully

--- example/s3_bucket_deployment.tf
+++ example/s3_bucket_deployment.tf
@@ -1,25 +1,25 @@
 ## Terrafom S3 bucket deployment module
 module "s3_bucket_deployment" {
-      source      = "../../terraform-s3-deployment"
+    source      = "../../terraform-s3-deployment"
 
-      name        = "my-test-bucket"
-      region      = "eu-west-1"
+    name        = "my-test-bucket.some.example.domain.com"
+    region      = "eu-west-1"
 
-##  Following data is optional, values below are an example:
-   
-      logging     = { log_bucket_id = "my-log-bucket"
-                      log_bucket_prefix = "my-test-bucket_logs/"
-                    }
-      life_cycle  = { days = 180
-                    }
-      replication = { replication_role_name = "my-log-bucket_replication_role"
-                      replication_policy_name = "my-log-bucket_replication_policy"
-                      replication_bucket_arn = "arn:aws:s3:::my-replication-bucket"
-                    }
-      versioning  = { enabled = "true"
-                    }
-      encryption  = { key_duration = 10
-                    }
-      iamuser     = true
+##  Following data is optional, values below are an example (uncomment and edit them for usage):
+#   
+#   logging     = { log_bucket_id = "my-logging-bucket.some.example.domain.com"
+#                   log_bucket_prefix = "my-test-bucket.some.example.domain.com_logs/"
+#                 }
+#   life_cycle  = { days = 180
+#                 }
+#   replication = { replication_role_name = "my-replication-bucket.some.example.domain.com_replication_role"
+#                   replication_policy_name = "my-replication-bucket.some.example.domain.com_policy"
+#                   replication_bucket_arn = "arn:aws:s3:::my-replication-bucket.some.example.domain.com"
+#                 }
+#   versioning  = { enabled = "true"
+#                 }
+#   encryption  = { key_duration = 7
+#                 }
+#   iamuser     = true
 
 }
